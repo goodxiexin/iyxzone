d4059632b7264a39d383360ed264a5627ac40d88
goodxiexin <xiexinwang@hotmail.com>
2010-03-11 10:30:27 +0800

    mailer style

--- app/mailer/friendship_mailer.rb
+++ app/mailer/friendship_mailer.rb
@@ -2,8 +2,8 @@ class FriendshipMailer < ActionMailer::Base
 
 	def request user, friend
 		setup_email friend
-		subject			"Dayday3 - #{user.login}要求加你为好友"
-		body				:friend => friend, :url => "#{SITE_URL}/requests"
+		subject			"17Gaming.com(一起游戏网) - #{user.login}要求加你为好友"
+		body				:user => user, :friend => friend, :url => "#{SITE_URL}/requests"
 	end
 
 protected
