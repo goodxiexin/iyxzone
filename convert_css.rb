bb69eafddec29ec06028cea0811911c76612cf42
gaoxh <gaoxh04@gmail.com>
2010-03-18 23:46:45 +0800

    convert css

--- convert_css.rb
+++ convert_css.rb
@@ -1,38 +1,55 @@
 require 'fileutils'
+
 RAILS_ROOT = '/home/gaoxh04/iyxzone'
-def add_timestamps_to_css_file file_name, dir_path
-  return if file_name == '.' or file_name == '..'
-  return if file_name.include? '?'
-  path = File.join(dir_path, file_name)
-  if File.directory? path 
-    Dir.new(path).each do |f|
-      add_timestamps_to_css_file f, path
+
+def correct_path file
+  # otherwise it's a deadin
+  return if File.basename(file) == '.' || File.basename(file) == '..'
+  
+  if File.directory? file
+    Dir.new(file).each do |f|
+      correct_path File.join(file, f)
     end
   else
-    puts path
+    return unless file =~ /\.css$/
     source = ''
-    File.open(path) {|f| source = f.read }
-    source.gsub!(/url\((['"]*)(.+)(['"]*)\)/) do
-      open, file, close = $1, $2, $3
-      #css_dir = "public/stylesheets"
-      timestamp = ''
-      FileUtils.cd("public/stylesheets") do
-        if file =~ /^\// # absolute path
-          f = File.new("#{RAILS_ROOT}/public" + file)
-        else # relative path
-          f = File.new("#{RAILS_ROOT}/#{File.join(dir_path, file)}")
-        end
-        timestamp = f.mtime.to_i.to_s
-      end
+    File.open(file) {|f| source = f.read}
+    source.gsub!(/url\(\.\.\/images/, "url(/images")
+    File.open(file, 'w') {|f| f.write(source)}
+  end
+end
 
-      if file =~ /.\.(ico|css|js|gif|jpe?g|png)/
-        "url(#{open}#{file}?#{timestamp}#{close})"
+def add_timestamps file
+  return if File.basename(file) == '.' || File.basename(file) == '..'
+
+  if File.directory? file
+    Dir.new(file).each do |f|
+      add_timestamps File.join(file, f)
+    end
+  else
+    return unless file =~ /\.css$/
+    source = ''
+    File.open(file) {|f| source = f.read }
+    source.gsub!(/url\((['"]*)(.+)(['"]*)\)/) do
+      open, fd, close = $1, $2, $3
+      timestamp = ''
+      if fd =~ /^\// # absolute path
+        f = File.new("public" + fd)
+      else # relative path
+        f = File.new(File.join(File.dirname(file), fd))
+      end
+      timestamp = f.mtime.to_i.to_s
+
+      if fd =~ /.\.(ico|css|js|gif|jpe?g|png)/
+        "url(#{open}#{fd}?#{timestamp}#{close})"
       else
-        "url(#{open}#{file}#{close})"
+        "url(#{open}#{fd}#{close})"
       end
     end
-    File.open("#{dir_path}/#{file_name}", 'w') {|f| f.write(source) }
+    File.open(file, 'w') {|f| f.write(source) }
   end 
 end
 
-add_timestamps_to_css_file '', 'public/stylesheets'
+correct_path "public/stylesheets"
+
+add_timestamps "public/stylesheets/themes/wow"
