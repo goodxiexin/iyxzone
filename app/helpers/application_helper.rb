b03bf2e7a3c5cf352a4bcc337664e7aed0636502
gaoxh <gaoxh04@gmail.com>
2010-03-24 20:57:33 +0800

    fix event form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -293,4 +293,23 @@ module ApplicationHelper
     link_to (image_tag "themes/#{skin.name}/#{skin.thumbnail}", {:alt => skin.name}.merge(img_opts)), skin_url(skin)
   end
 
+  # this function might be broken if rails gets updated
+  def onload_javascript_tag(content_or_options_with_block = nil, html_options = {}, &block)
+    content =
+      if block_given?
+        html_options = content_or_options_with_block if content_or_options_with_block.is_a?(Hash)
+        capture(&block)
+      else
+        content_or_options_with_block
+      end
+
+    tag = content_tag(:script, "document.observe('dom:loaded', function(){#{javascript_cdata_section(content)}});", html_options.merge(:type => Mime::JS))
+
+    if block_called_from_erb?(block)
+      concat(tag)
+    else
+      tag
+    end
+  end
+
 end
