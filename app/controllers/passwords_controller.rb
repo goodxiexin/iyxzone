6c5b75b6a33a9f0cbcb82e3b6c0fe02aae633e3d
goodxiexin <xiexinwang@hotmail.com>
2010-03-26 14:53:28 +0800

    mailer add link

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,7 +8,7 @@ class PasswordsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user.nil?
-      flash.now[:notice] = "该邮箱没有注册我们网站"
+      flash.now[:error] = "该邮箱没有注册我们网站"
       render :action => 'new'
     elsif !@user.active?
       redirect_to :controller => 'users', :action => 'activation_mail_sent', :show => 1, :email => @user.email
@@ -57,11 +57,11 @@ class PasswordsController < ApplicationController
           flash[:notice] = "密码已经重置"
           render '/login'
         else
-          flash[:notice] = "发生错误"
+          flash[:error] = "发生错误"
           render :action => 'edit', :password_reset_code => params[:password_reset_code]
         end
       else
-        flash.now[:notice] = "2此密码不匹配"
+        flash.now[:error] = "2此密码不匹配"
         render :action => 'edit', :password_reset_code => params[:password_reset_code]
         return
       end
