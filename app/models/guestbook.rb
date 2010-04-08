50b95cf5fd70ca2ea4d4f38fe00f4b73db3907ec
gaoxh <gaoxh04@gmail.com>
2010-04-08 17:53:47 +0800

    merge dyc

--- app/models/guestbook.rb
+++ app/models/guestbook.rb
@@ -1,5 +1,7 @@
 class Guestbook < ActiveRecord::Base
 
+  ErrorElements = ['日志', '视频','照片','状态','活动','投票','工会','分享','游戏','推荐游戏','每日十闻','首页','个人主页','好友','站内信','通知','邀请','设置',    '其它']
+
   belongs_to :user
 
   validates_presence_of :user_id, :message => "不能为空"
@@ -12,6 +14,18 @@ class Guestbook < ActiveRecord::Base
 
   validates_inclusion_of :priority, :in => [1, 2], :message => "只能是1或者2"
 
-  validates_inclusion_of :category, :in => 
+  validates_inclusion_of :category, :in => ErrorElements, :message => "类型不对" 
+
+  attr_readonly :user_id
+
+  attr_protected :reply, :done_date
+
+  attr_accessor :recently_reply
+
+  def reply_to_poster text
+    self.recently_reply = true
+    self.reply = text
+    self.save
+  end
 
 end
