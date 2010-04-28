20a7d9bc8a87c9d1477e10a3a5b2ca36ea2c2fc4
gaoxh <gaoxh04@gmail.com>
2010-04-28 15:15:44 +0800

    a

--- app/controllers/user/requests_controller.rb
+++ app/controllers/user/requests_controller.rb
@@ -5,16 +5,16 @@ class User::RequestsController < UserBaseController
 	def index
 		@requests = []
     if params[:type].blank?
-      @requests.concat current_user.event_requests
-      @requests.concat current_user.friend_requests
-      @requests.concat current_user.guild_requests
+      @requests.concat current_user.event_requests.all(:include => [:event, {:participant => :profile}])
+      @requests.concat current_user.friend_requests.all(:include => [:poll, {:user => :profile}])
+      @requests.concat current_user.guild_requests.all(:include => [:guild, {:user => :profile}])
       @requests = @requests.paginate :page => params[:page], :per_page => 10
     elsif params[:type].to_i == 1
-      @requests = current_user.friend_requests.paginate :page => params[:page], :per_page => 10
+      @requests = current_user.friend_requests.paginate :page => params[:page], :per_page => 10, :include => [:user, {:participant => :profile}]
     elsif params[:type].to_i == 2
-      @requests = current_user.event_requests.paginate :page => params[:page], :per_page => 10
+      @requests = current_user.event_requests.paginate :page => params[:page], :per_page => 10, :include => [:event, {:user => :profile}]
     elsif params[:type].to_i == 3
-      @requests = current_user.guild_requests.paginate :page => params[:page], :per_page => 10
+      @requests = current_user.guild_requests.paginate :page => params[:page], :per_page => 10, :include => [:guild, {:user => :profile}]
     end
 	end
 
