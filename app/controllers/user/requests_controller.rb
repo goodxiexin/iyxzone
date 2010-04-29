49ddf827276d8ea523d271777c742c2f7ac1659a
gaoxh <gaoxh04@gmail.com>
2010-04-29 13:39:33 +0800

    fix request prefetch bug

--- app/controllers/user/requests_controller.rb
+++ app/controllers/user/requests_controller.rb
@@ -6,13 +6,13 @@ class User::RequestsController < UserBaseController
 		@requests = []
     if params[:type].blank?
       @requests.concat current_user.event_requests.all(:include => [:event, {:participant => :profile}])
-      @requests.concat current_user.friend_requests.all(:include => [:poll, {:user => :profile}])
+      @requests.concat current_user.friend_requests.all(:include => [{:user => :profile}])
       @requests.concat current_user.guild_requests.all(:include => [:guild, {:user => :profile}])
       @requests = @requests.paginate :page => params[:page], :per_page => 10
     elsif params[:type].to_i == 1
-      @requests = current_user.friend_requests.paginate :page => params[:page], :per_page => 10, :include => [:user, {:participant => :profile}]
+      @requests = current_user.friend_requests.paginate :page => params[:page], :per_page => 10, :include => [{:user => :profile}]
     elsif params[:type].to_i == 2
-      @requests = current_user.event_requests.paginate :page => params[:page], :per_page => 10, :include => [:event, {:user => :profile}]
+      @requests = current_user.event_requests.paginate :page => params[:page], :per_page => 10, :include => [:event, {:participant => :profile}]
     elsif params[:type].to_i == 3
       @requests = current_user.guild_requests.paginate :page => params[:page], :per_page => 10, :include => [:guild, {:user => :profile}]
     end
