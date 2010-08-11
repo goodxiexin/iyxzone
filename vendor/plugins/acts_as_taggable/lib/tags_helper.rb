130d9a62abda08dfffac0ac7bd0311337e09c197
gaoxh <gaoxh04@gmail.com>
2010-08-11 16:09:16 +0800

    improve tag show

--- vendor/plugins/acts_as_taggable/lib/tags_helper.rb
+++ vendor/plugins/acts_as_taggable/lib/tags_helper.rb
@@ -2,12 +2,12 @@ module TagsHelper
 
 	def tag_cloud(taggable, classes)
 		return if taggable.nil?
-		tag_infos = taggable.taggings.group_by(&:tag).sort{rand}[0..14]
-		tags = tag_infos.map {|tag, taggings| tag}
-		tagging_counts = tag_infos.map {|tag, taggings| taggings.count}
+		tag_infos = taggable.taggings.group_by(&:tag_id).sort{rand}[0..14]
+		tag_ids = tag_infos.map {|tag_id, taggings| tag_id}
+		tagging_counts = tag_infos.map {|tag_id, taggings| taggings.count}
 		max_count = tagging_counts.max
     
-    tags.each_with_index do |tag, i|
+    Tag.match(:id => tag_ids).all.each_with_index do |tag, i|
       index = ((tagging_counts[i] / max_count) * (classes.size - 1)).round
       yield tag, classes[index]
     end
