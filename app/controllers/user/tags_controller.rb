771d7cc58d1775356a60db265ada5d145c294714
gaoxh <gaoxh04@gmail.com>
2010-08-12 15:24:36 +0800

    a

--- app/controllers/user/tags_controller.rb
+++ app/controllers/user/tags_controller.rb
@@ -1,18 +1,33 @@
 class User::TagsController < UserBaseController
 
+  def index
+    respond_to do |format|
+      format.html { render :partial => 'tag_cloud', :locals => {:taggable => @taggable, :deleteable => @taggable.is_tag_deleteable_by?(current_user)}}
+      format.json { render :json => @taggable.tags.map{|t| {:id => t.id, :name => t.name}} }
+    end
+  end
+
 	def create
+    if @taggable.add_tag current_user, params[:tag_name]
+      render :json => {:code => 1}
+    else
+      render :json => {:code => 0}
+    end
+=begin
     unless @taggable.add_tag current_user, params[:tag_name]
       render_js_error "发生错误，内容太长拉！"
     end
+=end
 	end
 
 	def destroy
     @tag = Tag.find(params[:id])
 		
     if @taggable.destroy_tag @tag.name 
-			render_js_code "$('tag_#{@tag.id}').remove();"
+			render :json => {:code => 1} #render_js_code "$('tag_#{@tag.id}').remove();"
 		else
-      render_js_error
+      render :json => {:code => 0}
+      #render_js_error
     end
 	end
 
@@ -24,8 +39,8 @@ class User::TagsController < UserBaseController
 protected
 
 	def setup
-    if ["create"].include? params[:action]
-      @taggable = get_taggable
+    if ["index", "create"].include? params[:action]
+      @taggable = params[:taggable_type].camelize.constantize.find(params[:taggable_id])
       require_create_privilege @taggable
 		elsif ["destroy"].include? params[:action]
       @taggable = get_taggable
