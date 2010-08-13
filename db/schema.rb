56fe23b832aef1440dc9fedf7e54ab3a7d7bd3fe
goodxiexin <xiexinwang@hotmail.com>
2010-08-13 15:50:30 +0800

    add games update

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100805150042) do
+ActiveRecord::Schema.define(:version => 20100813041259) do
 
   create_table "albums", :force => true do |t|
     t.string   "type"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(:version => 20100805150042) do
     t.string   "name"
     t.text     "about"
     t.integer  "comments_count", :default => 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "attentions", :force => true do |t|
+    t.integer  "attentionable_id"
+    t.string   "attentionable_type"
+    t.integer  "follower_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -403,6 +411,10 @@ ActiveRecord::Schema.define(:version => 20100805150042) do
     t.datetime "updated_at"
   end
 
+  create_table "links", :force => true do |t|
+    t.string "url"
+  end
+
   create_table "mails", :force => true do |t|
     t.integer  "sender_id"
     t.integer  "recipient_id"
@@ -500,8 +512,7 @@ ActiveRecord::Schema.define(:version => 20100805150042) do
   end
 
   create_table "mini_topics", :force => true do |t|
-    t.string  "name"
-    t.integer "freq", :default => 0
+    t.string "name"
   end
 
   create_table "news", :force => true do |t|
@@ -751,6 +762,29 @@ ActiveRecord::Schema.define(:version => 20100805150042) do
     t.datetime "updated_at"
   end
 
+  create_table "shares", :force => true do |t|
+    t.integer  "shareable_id"
+    t.string   "shareable_type"
+    t.integer  "digs_count",     :default => 0
+    t.integer  "sharings_count", :default => 0
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sharings", :force => true do |t|
+    t.string   "title"
+    t.text     "reason"
+    t.string   "shareable_type"
+    t.integer  "share_id"
+    t.integer  "poster_id"
+    t.integer  "comments_count", :default => 0
+    t.datetime "created_at"
+    t.integer  "verified",       :default => 0
+  end
+
+  add_index "sharings", ["poster_id"], :name => "index_sharings_on_poster_id"
+
   create_table "signup_invitations", :force => true do |t|
     t.integer  "sender_id"
     t.string   "recipient_email"
@@ -770,6 +804,17 @@ ActiveRecord::Schema.define(:version => 20100805150042) do
     t.text     "access_list"
     t.string   "directory"
   end
+
+  create_table "statuses", :force => true do |t|
+    t.integer  "poster_id"
+    t.text     "content"
+    t.integer  "comments_count", :default => 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "verified",       :default => 0
+  end
+
+  add_index "statuses", ["poster_id"], :name => "index_statuses_on_poster_id"
 
   create_table "subdomains", :force => true do |t|
     t.integer "user_id"
